c4e8d4a3479f091169ced452fb1ca9bea6e94b7d
EmericBayard <57359277+EmericBayard@users.noreply.github.com>
2021-09-21 18:51:15 +0200

    📦 NEW: unitary testing with jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,14 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: './custom-jest-environment.js',
-    roots: ["./src"],
-    transform: {
-        "^.+\\.jsx?$": "babel-jest", 
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    roots: [
+      "<rootDir>/src"
+    ],
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-        "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
-    },
-    globals: {
-    },
-  };
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/__mocks__/fileMock.js',
+  '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    }
+};
 
 export {}
